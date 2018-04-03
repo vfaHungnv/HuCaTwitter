e130664f2e2d0ff3fa4217b79c980ef4c3493243
HungNV <hungnv@vitalify.asia>
2018-04-03 11:51:45 +0700

    #singleton and completion block.

--- hucatwitter/hucatwitter/Controller/HomeDatasourceController.swift
+++ hucatwitter/hucatwitter/Controller/HomeDatasourceController.swift
@@ -23,50 +23,8 @@ class HomeDatasourceController: DatasourceController {
         
         setupNavigationBarItems()
         
-//        let homeDatasource = HomeDatasource()
-//        self.datasource = homeDatasource
-        
-        fetchHomeFeed()
-        Service.sharedInstance.fetchHomeFeed()
-    }
-    
-    let tron = TRON(baseURL: "http://api.letsbuildthatapp.com")
-    
-    class Home: JSONDecodable {
-
-        let users: [User]
-        required init(json: JSON) throws {
-            
-            var users = [User]()
-            let array = json["users"].array
-            for userJson in array! {
-                let name = userJson["name"].stringValue
-                let username = userJson["username"].stringValue
-                let bio = userJson["bio"].stringValue
-                
-                let user = User(name: name, username: username, bioText: bio, profileImage: UIImage())
-                
-                users.append(user)
-            }
-            self.users = users
-        }
-    }
-    
-    class JSONError: JSONDecodable {
-        required init(json: JSON) throws {
-            print("JSON Error")
-        }
-    }
-    
-    fileprivate func fetchHomeFeed() {
-        let request: APIRequest<HomeDatasource, JSONError> = tron.swiftyJSON.request("/twitter/home")
-        request.perform(withSuccess: { (homeDatasource) in
-            
-            print(homeDatasource.users.count)
-            
+        Service.sharedInstance.fetchHomeFeed { (homeDatasource) in
             self.datasource = homeDatasource
-        }) { (err) in
-            print("Failed to fetch json...", err)
         }
     }
     
--- hucatwitter/hucatwitter/Service/Service.swift
+++ hucatwitter/hucatwitter/Service/Service.swift
@@ -7,11 +7,28 @@
 //
 
 import Foundation
+import TRON
+import SwiftyJSON
 
 struct Service {
     static let sharedInstance = Service()
     
-    func fetchHomeFeed() {
-        
+    let tron = TRON(baseURL: "https://api.letsbuildthatapp.com")
+    
+    func fetchHomeFeed(completion: @escaping (HomeDatasource) -> ()) {
+        let request: APIRequest<HomeDatasource, JSONError> = tron.swiftyJSON.request("/twitter/home")
+        request.perform(withSuccess: { (homeDatasource) in
+            
+            completion(homeDatasource)
+            
+        }) { (err) in
+            print("Failed to fetch json...", err)
+        }
+    }
+    
+    class JSONError: JSONDecodable {
+        required init(json: JSON) throws {
+            print("JSON Error")
+        }
     }
 }
